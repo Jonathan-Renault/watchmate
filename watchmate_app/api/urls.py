c9ce6ca618b7ade6989397fbd9034971e9980a14
Jonathan-Renault <jon.renault@gmail.com>
2021-09-28 11:35:12 +0200

    Course end 20

--- watchmate_app/api/urls.py
+++ watchmate_app/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from watchmate_app.api.views import movie_list, movie_detail
+#from watchmate_app.api.views import movie_list, movie_detail
+from watchmate_app.api.views import MovieListAV, MovieDetailAV
+
+
+# urlpatterns = [
+#     path('list/', movie_list, name='movie-list'),
+#     path('<int:pk>/', movie_detail, name='movie-detail'),
+# ]
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>/', movie_detail, name='movie-detail'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie-detail'),
 ]
